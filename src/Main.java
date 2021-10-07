04444e4fa6882aaf3096eaf35ac65b61e5262bdb
SchekAlex <ofmb1953@gmail.com>
2021-10-07 23:34:29 +0300

    base

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
-public class Main {
+import java.lang.reflect.Method;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main{
     public static void main(String[] args) {
+        Methods method = new Methods();
+        String randomWord = "Automation";
+        method.backwardsString(randomWord);
+
+        List<String> list =new ArrayList<>();
+        String element1 = "Hello";
+        String element2 = "Qualitest";
+        String element3 = "in";
+        String element4 = "a";
+        String element5 = "list";
+
+        list.add(element1);
+        list.add(element2);
+        list.add(element3);
+        list.add(element4);
+        list.add(element5);
+
+        method.printList(list);
+
+
+
 
     }
 }
--- src/Methods.java
+++ src/Methods.java
@@ -1,2 +1,39 @@
+import java.util.List;
+
 public class Methods {
+//    Exercise 1:
+//    Define a function that receive a string as argument
+//    and prints it backwards. For example, the string "Automation"
+//    gets printed as : "noitamotuA"
+    public void backwardsString(String word){
+        char[] basicWord = word.toCharArray();
+        for (int i=basicWord.length-1; i>=0; i--){
+            System.out.print(basicWord[i]);
+        }
+    }
+
+    public void printList(List<String> wordList){
+        System.out.println();
+        String firstWord = wordList.get(0);
+        for(int i = 0; i<=firstWord.length()+2;i++){
+            System.out.print("* ");
+
+        }
+
+        System.out.println();
+
+        int nrOfElements = wordList.size();
+        for(int i = 0; i<nrOfElements;i++){
+            System.out.print("* "+" "+ wordList.get(i)+" "+ " *");
+            System.out.println();
+        }
+
+        for(int i = 0; i<=firstWord.length()+2;i++){
+            System.out.print("* ");
+
+        }
+    }
+
+
+
 }
